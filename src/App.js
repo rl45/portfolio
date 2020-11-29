dd5b26a1b1d663461bc547ffd355000f9cf22056
Raymond Lee <raymondlee@Raymonds-iMac.local>
2020-11-28 23:59:51 -0800

    resume

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ class App extends React.Component {
     this.state = {
       tesla: {
         image: Teslaimage,
-        description: "Came across a top design on Behance, and approached it through code. Took screenshots, cut out each component is Sketch labeling grid percentages, padding, margins, and overall layout of each component. Built the layout using React and separated the components into BottomBar, MusicView, PlayBar, SecondaryBar, and TopBar. Added an animation that allowed the user to swipe up on the playbar showcasing the MusicView. Captured the user's window height and subtracted it from the user's Y mouse position while setting boundaries to then change the CSS height of the component resulting in a seamless swipe.",
+        description: "Came across a top design on Behance, and approached it through code [Not mobile responsive due to Tesla's static screen]. Took screenshots, cut out each component is Sketch labeling grid percentages, padding, margins, and overall layout of each component. Built the layout using React and separated the components into BottomBar, MusicView, PlayBar, SecondaryBar, and TopBar. Added an animation that allowed the user to swipe up on the playbar showcasing the MusicView. Captured the user's window height and subtracted it from the user's Y mouse position while setting boundaries to then change the CSS height of the component resulting in a seamless swipe.",
         tools: ["ReactJS", "CSS", "Mouse Events", "Grid", "React-Feathers", "Sketch"],
-        githubLink: "https://5fc35081c2e60873f54d31a2--tesla-interface.netlify.app",
+        githubLink: "https://github.com/rl45/tesla",
         websiteLink: "https://5fc35081c2e60873f54d31a2--tesla-interface.netlify.app",
         name: "tesla"
       },
